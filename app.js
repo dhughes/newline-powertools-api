ec71285d80feffa7be4b72bc31f756fcc8f98329
Doug Hughes <doug@doughughes.net>
2017-07-12 22:22:22 -0400

    updates so that this (almost) works on aws.

--- app.js
+++ app.js
@@ -33,50 +33,11 @@ app.post('/project', (req, res) => {
     // sync to a github repo
     .sync(req.body)
     // return the result
-    .then(result => res.json(result));
-
-  // // parse the project json
-
-  // Promise
-  //   // the specified object will hold the state of our promise chain as we move through it.
-  //   .resolve({ project, slug, workingDir, expectedMessage })
-  //   .then(state => console.log(state))
-  //   .then(state => res.json(state));
-
-  // note: this started out feeling ok, but now it feels rather unclean. Consider refactoring.
-  // repo
-  //   .assertExists(slug)
-  //   // this catches a situation where the repository does not exist.
-  //   .catch({ message: 'REPO_DOES_NOT_EXIST' }, e => repo.create(slug))
-  //   // if there was an error creating the repo
-  //   .catch({ message: 'ERROR_CREATING_REPO' }, e => console.log('There was an error creating the github repository.'))
-  //   /*
-  //     The repo exists by this point. Now we need to start iterating through the
-  //     files to see if any have not been committed to GH
-  //   */
-  //
-  //   // have any of our files been changed since the last commit, if any?
-  //   .then(() => repo.assertModified(slug, expectedMessage))
-  //   // if we have changes then we need to clone the remote repo
-  //   .then(() => repo.clone(slug, workingDir))
-  //   // next, we'll download the files from newline into our project/repo
-  //   // let's get these file into the scope of this promise chain
-  //   .then(() => project.files)
-  //   // download all files
-  //   .map(file => fileService.downloadFile(file))
-  //   // unzip the files (note: I don't like how I'm passing the files array through this chain. it feels weird.)
-  //   .then(files => fileService.unzip(files))
-  //   // add and commit changed files to git
-  //   .then(files => repo.add(workingDir).then(() => files))
-  //   // commit any changes
-  //   .then(files => repo.commit(workingDir, expectedMessage).then(() => files))
-  //   // send a response to the client
-  //   .then(files => {
-  //     res.json(files);
-  //     return files;
-  //   })
-  //   // log
-  //   .then(files => console.log(files));
+    .then(result => res.json(result))
+    // if there are any errors, be sure to catch them
+    .catch(e => {
+      res.status(500).json(e);
+    });
 });
 
 // The aws-serverless-express library creates a server and listens on a Unix
@@ -85,27 +46,3 @@ app.post('/project', (req, res) => {
 
 // Export your express server so you can import it in the lambda function.
 module.exports = app;
-
-// var walk = Promise.promisify(function(dir, done) {
-//   var results = [];
-//   fs.readdir(dir, function(err, list) {
-//     if (err) return done(err);
-//     var pending = list.length;
-//     if (!pending) return done(null, results);
-//     list.forEach(function(file) {
-//       file = path.resolve(dir, file);
-//       fs.stat(file, function(err, stat) {
-//         if (stat && stat.isDirectory()) {
-//           walk(file, function(err, res) {
-//             results = results.concat(res);
-//             if (!--pending) done(null, results);
-//           });
-//         } else {
-//           results.push(file);
-//           if (!--pending) done(null, results);
-//         }
-//       });
-//     });
-//   });
-// });
-// end temp
--- projectService.js
+++ projectService.js
@@ -2,11 +2,14 @@ const prettyjson = require('prettyjson');
 const Promise = require('bluebird');
 const request = Promise.promisify(require('request'));
 const download = Promise.promisify(require('download-file'));
+const which = Promise.promisify(require('which'));
 const fs = require('fs-extra');
 const Repository = require('git-cli').Repository;
 const uuid = require('uuid/v4');
 const merge = require('lodash/merge');
 const unzipper = require('unzipper');
+const path = require('path');
+const tar = require('tar');
 
 function ProjectService() {
   /**
@@ -18,6 +21,7 @@ function ProjectService() {
     // constants
     const githubUsername = 'dhughes';
     const tempDir = '/tmp/projects';
+    const tempGit = '/tmp/git';
     const githubApiToken = process.env.GITHUB_API_TOKEN;
     const apiRoot = 'https://api.github.com';
     const githubRoot = `https://${githubUsername}:${githubApiToken}@github.com/`;
@@ -29,9 +33,12 @@ function ProjectService() {
     const workingDir = `${tempDir}/${slug}`;
     const expectedMessage = project.files.map(file => file.uniqueName).sort().toString();
 
+    //this.walkPromise('/tmp/git').then(res => console.log(res));
+
     return (
       Promise.resolve({
         tempDir,
+        tempGit,
         githubApiToken,
         apiRoot,
         token,
@@ -42,8 +49,12 @@ function ProjectService() {
         expectedMessage,
         githubRoot
       })
+        //
+        .then(state => this.ensureGitIsInstalled(state))
+        // log our environment
+        .then(state => this.log(state, process.env, 'Environment Vars'))
         // make sure our project exists as a github repo
-        .then(state => this.insureGithubRepoExists(state))
+        .then(state => this.ensureGithubRepoExists(state))
         // check if github is up to date. If not, we sync!
         .then(state =>
           this.checkIfGithubIsUpToDate(state).then(state => {
@@ -52,9 +63,66 @@ function ProjectService() {
           })
         )
         // log our state
-        .then(state => this.log(state))
+        .then(state => this.log(state, state, 'State'))
+        // catch any errors
+        .catch(error => this.log(state, error, 'sync/Error!'))
     );
   };
+
+  /**
+   * This function ensures that Git is installed
+   * @param  {[type]} state [description]
+   * @return {[type]}       [description]
+   */
+  this.ensureGitIsInstalled = state =>
+    this.log(state, 'Ensuring Git is Installed')
+      // which git
+      .then(() => which('git'))
+      // if git is found then print its location
+      .then(res => this.log(state, res, 'Git is Installed At'))
+      // if git is not found then install it
+      .catch(() => this.log(state, 'Git is NOT Installed').then(() => this.installGit(state)))
+      // return the state
+      .then(() => state);
+
+  this.installGit = state =>
+    this.log(state, 'Installing Git')
+      .then(() => fs.ensureDir(state.tempGit))
+      // extract the tar for git
+      .then(() =>
+        tar
+          .extract({
+            cwd: state.tempGit,
+            file: path.join(__dirname, 'git-2.4.3.tar')
+          })
+          // add git to the path
+          .then(() => this.addGitToPath(state))
+          .catch(error => {
+            console.log('there was an error extracting git', error);
+            return state;
+          })
+      )
+      .catch(error => this.log(state, error, 'installGit/Error!'));
+
+  this.addGitToPath = state =>
+    this.log(state, 'Adding Git to Path')
+      // add git to the path
+      .then(() => {
+        const GIT_TEMPLATE_DIR = path.join(state.tempGit, '/usr/share/git-core/templates');
+        const GIT_EXEC_PATH = path.join(state.tempGit, '/usr/libexec/git-core');
+        const binPath = path.join(state.tempGit, '/usr/bin');
+
+        process.env.PATH = `${process.env.PATH}:${binPath}`;
+        process.env.GIT_TEMPLATE_DIR = GIT_TEMPLATE_DIR;
+        process.env.GIT_EXEC_PATH = GIT_EXEC_PATH;
+
+        return state;
+      })
+      // which git
+      .then(() => which('git'))
+      .then(res => this.log(state, res, 'Git is Installed At'))
+      .catch(error => this.log(state, error, 'addGitToPath/Error!'))
+      .then(() => state);
 
   /**
    * This performs all the actions of actively syncing the zip files to github
@@ -62,44 +130,58 @@ function ProjectService() {
    * @return {[type]}       [description]
    */
   this.performSync = state =>
-    this.removeProjectDirectory(state)
-      // clone the github repo
-      .then(state => this.cloneGithubRepo(state))
-      // empty the project directory (except for .git)
-      .then(state => this.emptyProjectDirectory(state))
-      // download the zip files
-      .then(state => this.downloadZips(state))
-      // extract the zip files
-      .then(state => this.extractZips(state))
-      // remove the zip files and the __MACOSX folder
-      .then(state => this.cleanupZips(state))
-      // add the changed files
-      .then(state => this.gitAdd(state))
-      // commit the changes
-      .then(state => this.gitCommit(state))
-      // push the changes to github
-      .then(state => this.pushToGithub(state));
+    this.log(state, 'Performing Sync To Github...').then(() =>
+      this.removeProjectDirectory(state)
+        // clone the github repo
+        .then(state => this.cloneGithubRepo(state))
+        // empty the project directory (except for .git)
+        .then(state => this.emptyProjectDirectory(state))
+        // download the zip files
+        .then(state => this.downloadZips(state))
+        // extract the zip files
+        .then(state => this.extractZips(state))
+        // remove the zip files and the __MACOSX folder
+        .then(state => this.cleanupZips(state))
+        // add the changed files
+        .then(state => this.gitAdd(state))
+        // commit the changes
+        .then(state => this.gitCommit(state))
+        // push the changes to github
+        .then(state => this.pushToGithub(state))
+    );
 
   /**
    * Push master to github
    * @param  {[type]} state [description]
    * @return {[type]}       [description]
    */
-  this.pushToGithub = state => new Repository(state.workingDir + '/.git').push().then(() => state);
+  this.pushToGithub = state =>
+    this.log(state, 'Pushing to Github')
+      .then(() => new Repository(state.workingDir + '/.git').push())
+      .catch(error => this.log(state, error, 'pushToGithub/Error!'))
+      .then(() => state);
 
   /**
    * This commits the changes to git using the correct commit message
    * @param  {[type]} state [description]
    * @return {[type]}       [description]
    */
-  this.gitCommit = state => new Repository(state.workingDir + '/.git').commit(state.expectedMessage).then(() => state);
+  this.gitCommit = state =>
+    this.log(state, 'Committing to Local Repo')
+      .then(() => new Repository(state.workingDir + '/.git').commit(state.expectedMessage))
+      .catch(error => this.log(state, error, 'gitCommit/Error!'))
+      .then(() => state);
 
   /**
    * This adds any changed files to git
    * @param  {[type]} state [description]
    * @return {[type]}       [description]
    */
-  this.gitAdd = state => new Repository(state.workingDir + '/.git').add().then(() => state);
+  this.gitAdd = state =>
+    this.log(state, 'Adding to Local Repo')
+      .then(() => new Repository(state.workingDir + '/.git').add())
+      .catch(error => this.log(state, error, 'gitAdd/Error!'))
+      .then(() => state);
 
   /**
    * This deletes the zip files
@@ -107,9 +189,12 @@ function ProjectService() {
    * @return {[type]}       [description]
    */
   this.cleanupZips = state =>
-    Promise.map(state.project.files, file => fs.remove(file.tempFile))
-      .then(() => fs.remove(`${state.workingDir}/__MACOSX`))
-      .then(() => state);
+    this.log(state, 'Clean up Zip Files').then(() =>
+      Promise.map(state.project.files, file => fs.remove(file.tempFile))
+        .then(() => fs.remove(`${state.workingDir}/__MACOSX`))
+        .catch(error => this.log(state, error, 'cleanupZips/Error!'))
+        .then(() => state)
+    );
 
   /**
    * This extracts a zip file
@@ -117,9 +202,13 @@ function ProjectService() {
    * @return {[type]}       [description]
    */
   this.extractZips = state =>
-    Promise.map(state.project.files, file =>
-      fs.createReadStream(file.tempFile).pipe(unzipper.Extract({ path: state.workingDir })).promise()
-    ).then(() => state);
+    this.log(state, 'Extract Zip Files').then(() =>
+      Promise.map(state.project.files, file =>
+        fs.createReadStream(file.tempFile).pipe(unzipper.Extract({ path: state.workingDir })).promise()
+      )
+        .catch(error => this.log(state, error, 'extractZips/Error!'))
+        .then(() => state)
+    );
 
   /**
    * This downloads the zips for the project into a temporary directory
@@ -127,11 +216,15 @@ function ProjectService() {
    * @return {[type]}       [description]
    */
   this.downloadZips = state =>
-    Promise.map(state.project.files, file =>
-      download(file.url, { directory: state.workingDir, filename: file.uniqueName }).then(tempFile =>
-        merge({}, file, { tempFile })
+    this.log(state, 'Download Zip Files').then(() =>
+      Promise.map(state.project.files, file =>
+        download(file.url, { directory: state.workingDir, filename: file.uniqueName }).then(tempFile =>
+          merge({}, file, { tempFile })
+        )
       )
-    ).then(files => merge({}, state, { project: { files } }));
+        .catch(error => this.log(state, error, 'downloadZips/Error!'))
+        .then(files => merge({}, state, { project: { files } }))
+    );
 
   /**
    * This checks to see if github is up to date with the latest zip files
@@ -139,42 +232,48 @@ function ProjectService() {
    * @return {[type]}       [description]
    */
   this.checkIfGithubIsUpToDate = state =>
-    get(`${state.apiRoot}/repos/${state.org}/${state.slug}/commits?${state.token}`).then(response => {
-      if (response.statusCode === 200) {
-        // we are expecting that the files have been changed. not not then we throw a message
-        if (JSON.parse(response.body).map(commit => commit.commit.message).pop() === state.expectedMessage) {
-          return merge({}, state, { githubUpToDate: true });
-        }
+    this.log(state, 'Checking if Github is Up To Date').then(() =>
+      get(`${state.apiRoot}/repos/${state.org}/${state.slug}/commits?${state.token}`)
+        .then(response => {
+          if (response.statusCode === 200) {
+            // we are expecting that the files have been changed. if not then we throw a message
+            if (JSON.parse(response.body).map(commit => commit.commit.message).pop() === state.expectedMessage) {
+              return merge({}, state, { githubUpToDate: true });
+            }
 
-        // 409 means there have been no commits yet, thus there are changes. Anything else is an error.
-      } else if (response.statusCode !== 409) {
-        throw new Error(
-          `Unable to confirm whether or not ${state.slug} contains the current file information. IE: I couldn't check the logs. HTTP status: ${response.statusCode}`
-        );
-      }
+            // 409 means there have been no commits yet, thus there are changes. Anything else is an error.
+          } else if (response.statusCode !== 409) {
+            throw new Error(
+              `Unable to confirm whether or not ${state.slug} contains the current file information. IE: I couldn't check the logs. HTTP status: ${response.statusCode}`
+            );
+          }
 
-      return merge({}, state, { githubUpToDate: false });
-    });
+          return merge({}, state, { githubUpToDate: false });
+        })
+        .catch(error => this.log(state, error, 'checkIfGithubIsUpToDate/Error!'))
+    );
 
   /**
    * Deletes everything from the project directory except for the .git folder.
    * @param  {[type]} state [description]
    * @return {[type]}       [description]
    */
-  this.emptyProjectDirectory = state => {
-    const tempGitFolderName = uuid();
+  this.emptyProjectDirectory = state =>
+    this.log(state, 'Emptying Project Directory').then(() => {
+      const tempGitFolderName = uuid();
 
-    return (
-      fs
-        //temporarily move the .git folder
-        .move(`${state.workingDir}/.git`, `${state.workingDir}/../${tempGitFolderName}`)
-        // remove all the files from the working directory
-        .then(() => fs.emptyDir(state.workingDir))
-        // move the .git directory back into place
-        .then(() => fs.move(`${state.workingDir}/../${tempGitFolderName}`, `${state.workingDir}/.git`))
-        .then(() => state)
-    );
-  };
+      return (
+        fs
+          //temporarily move the .git folder
+          .move(`${state.workingDir}/.git`, `${state.workingDir}/../${tempGitFolderName}`)
+          // remove all the files from the working directory
+          .then(() => fs.emptyDir(state.workingDir))
+          // move the .git directory back into place
+          .then(() => fs.move(`${state.workingDir}/../${tempGitFolderName}`, `${state.workingDir}/.git`))
+          .catch(error => this.log(state, error, 'emptyProjectDirectory/Error!'))
+          .then(() => state)
+      );
+    });
 
   /**
    * This clones a github repository to the a working directory
@@ -182,58 +281,76 @@ function ProjectService() {
    * @return {[type]}       [description]
    */
   this.cloneGithubRepo = state =>
-    Repository.clone(`${state.githubRoot}${state.org}/${state.slug}`, state.workingDir).then(() => state);
+    this.log(state, 'Cloning Github Repo')
+      .then(() => Repository.clone(`${state.githubRoot}${state.org}/${state.slug}`, state.workingDir))
+      .catch(error => this.log(state, error, 'cloneGithubRepo/Error!'))
+      .then(() => state);
 
   /**
    * Remove the project's working directory if it exists
    * @param  {} state   the current state object
    * @return {}         the current state object
    */
-  this.removeProjectDirectory = state => fs.remove(state.workingDir).then(() => state);
+  this.removeProjectDirectory = state =>
+    this.log(state, 'Remove Project Working Directory')
+      .then(() => fs.remove(state.workingDir))
+      .catch(error => this.log(state, error, 'removeProjectDirectory/Error!'))
+      .then(() => state);
 
   /**
    * makes sure a github repository exists for this project
    * @param  {} state   the current state object
    * @return {}         the current state object
    */
-  this.insureGithubRepoExists = state =>
-    get(`${state.apiRoot}/repos/${state.org}/${state.slug}?${state.token}`).then(response => {
-      if (response.statusCode === 200) {
-        return state;
-      } else if (response.statusCode === 404) {
-        return this.createRepository(state);
-      } else {
-        throw new Error(`Unable to confirm ${state.slug} exists on Github. HTTP status: ${response.statusCode}`);
-      }
-    });
+  this.ensureGithubRepoExists = state =>
+    this.log(state, 'Ensuring Github Repo Exists')
+      .then(() =>
+        get(`${state.apiRoot}/repos/${state.org}/${state.slug}?${state.token}`).then(response => {
+          if (response.statusCode === 200) {
+            return this.log(state, 'Github Repo Happily Exists');
+          } else if (response.statusCode === 404) {
+            return this.createRepository(state);
+          } else {
+            throw new Error(`Unable to confirm ${state.slug} exists on Github. HTTP status: ${response.statusCode}`);
+          }
+        })
+      )
+      .catch(error => this.log(state, error, 'ensureGithubRepoExists/Error!'));
 
   /**
    * Creates a repository on Github
    * @param  {} state   the current state object
    * @return {}         the current state object
    */
-  this.createRepository = state => {
-    const repository = {
-      name: state.slug
-    };
+  this.createRepository = state =>
+    this.log(state, 'Creating Github Repo')
+      .then(() => {
+        const repository = {
+          name: state.slug
+        };
 
-    return post(`${state.apiRoot}/orgs/${state.org}/repos?${state.token}`, repository).then(response => {
-      if (response.statusCode === 201) {
-        return state;
-      } else {
-        throw new Error(`Unable to create new github repository '${state.slug}'. HTTP status: ${response.statusCode}`);
-      }
-    });
-  };
+        return post(`${state.apiRoot}/orgs/${state.org}/repos?${state.token}`, repository).then(response => {
+          if (response.statusCode === 201) {
+            return state;
+          } else {
+            throw new Error(
+              `Unable to create new github repository '${state.slug}'. HTTP status: ${response.statusCode}`
+            );
+          }
+        });
+      })
+      .catch(error => this.log(state, error, 'createRepository/Error!'));
 
   /**
    * Logs the state to the console
    * @param  {} state   the current state object
    * @return {}         the current state object
    */
-  this.log = state => {
-    console.log(prettyjson.render(state));
-    return state;
+  this.log = (state, thing = state, label = '') => {
+    console.log(`-----${label}-----`);
+    console.log(prettyjson.render(thing));
+    console.log(`-----${label.replace(/./g, '-')}-----`);
+    return Promise.resolve(state);
   };
 
   /**
@@ -242,7 +359,6 @@ function ProjectService() {
    * @return {[type]}     [description]
    */
   function get(url) {
-    console.log('GET: ' + url);
     return request({
       url: url,
       method: 'GET',
@@ -259,7 +375,6 @@ function ProjectService() {
    * @return {[type]}      [description]
    */
   function post(url, body) {
-    console.log('POST: ' + url);
     return request({
       url: url,
       method: 'POST',
@@ -267,6 +382,31 @@ function ProjectService() {
       headers: {
         'User-Agent': 'newline-powertools-api'
       }
+    });
+  }
+
+  this.walkPromise = Promise.promisify(walk);
+
+  function walk(dir, done) {
+    var results = [];
+    fs.readdir(dir, function(err, list) {
+      if (err) return done(err);
+      var pending = list.length;
+      if (!pending) return done(null, results);
+      list.forEach(function(file) {
+        file = path.resolve(dir, file);
+        fs.stat(file, function(err, stat) {
+          if (stat && stat.isDirectory()) {
+            walk(file, function(err, res) {
+              results = results.concat(res);
+              if (!--pending) done(null, results);
+            });
+          } else {
+            results.push(file);
+            if (!--pending) done(null, results);
+          }
+        });
+      });
     });
   }
 }
